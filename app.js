4ebb4bd9b786210af870b60a7f976891c6d27eb9
hmshujaatali <81732578+hmshujaatali@users.noreply.github.com>
2021-08-18 15:09:23 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,105 @@
+//              QUESTION#01
+// var input=prompt("ENTER CHAR OR NUM")
+// if(isNaN(input))
+// {
+// if(input>="A" & input<="Z"){
+//     alert("upPercase"
+// }
+// if(input>="a" & input<="z"){
+//     alert("lower")
+// }
+// }else{
+//     alert("Its a Numebr: ",input)
+// }
+//              QUESTION#02
+// var int1=+prompt("Enter 1st Integer:")
+// var int2=+prompt("Enter 2nd Integer:")
+// if(int1>int2){
+//     document.write("Integer 1 is greater: ",int1)
+// }else
+// if(int2>int1){
+//     document.write("Integer 2 is greater: ",int2)
+// }else{
+//     document.write("Both Integers are equal: ",int1," ",int2)
+// }
+//              QUESTION#03
+// var input=+prompt("ENTER A NUMBER")
+// if(input>0){
+//     alert("Num is positive")
+// }else
+// if(input<0){
+//     alert("Num iS negative")
+// }else{
+//     alert("Number equal to zeroooooooooooooooooooooooooo")
+// }
+//              QUESTION#04
+// var char=prompt("Enter only 1 Variable:")
+// if(char=="a"&&"i"&&"o"&&"u"&&"e"){
+//     alert("Its a vowel char")
+// }else{
+//     alert("its not a vowel")
+// }
+//              QUESTION#05
+// var offpassword="a1"
+// var onpassword=prompt("Enter a Required PASSWORD")
+// if(onpassword===offpassword){
+// alert("Your password is correct!")
+// }else{
+//     alert("wrong password!")
+// }
+//              QUESTION#06
+// var greeting;
+// var hour = 13;
+// if (hour > 18) {
+// greeting= "Good day";
+// alert(greeting)
+// }else{
+// greeting = "Good evening";
+// alert(greeting)
+// }
+//           QUESTION#07
+// var time=+prompt("enter time: ")
+// if(time==1300){
+//     alert("time is 0100P.M")
+// }else
+// if(time==1400){
+//     alert("time is 0200P.M")
+// }else
+// if(time==1500){
+//     alert("time is 0300P.M")
+// }else
+// if(time==1600){
+//     alert("time is 0400P.M")
+// }else
+// if(time==1700){
+//     alert("time is 0500P.M")
+// }else
+// if(time==1800){
+//     alert("time is 0600P.M")
+// }else
+// if(time==1900){
+//     alert("time is 0700P.M")
+// }else
+// if(time==2000){
+//     alert("time is 0800P.M")
+// }else
+// if(time==2100){
+//     alert("time is 0900P.M")
+// }else
+// if(time==2200){
+//     alert("time is 1000P.M")
+// }else
+// if(time==2300){
+//     alert("time is 1100P.M")
+// }else
+// if(time==2400){
+//     alert("time is 1200A.M")
+// }else{
+//     alert("Time Error")
+// }
+
+
+
+
+
+
